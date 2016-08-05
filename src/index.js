627304b263267cbe7e629169970f77fdf6313244
Scott Wade <sc0ttwad3@gmail.com>
2016-08-04 23:38:57 -0400

    Add string template to index page and add moment library to project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import moment from 'moment';
 import App from './App';
 
 
@@ -9,3 +9,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 // Verify ES2016
 console.log(`2 + 8 is ${2+8}`);
+console.log('Hello... it\'s ' + moment().format('h:mm:ss a') + '!');
+let current = moment().format('h:mm:ss a')
+console.log(`Hello... it\'s ${current}!`);
